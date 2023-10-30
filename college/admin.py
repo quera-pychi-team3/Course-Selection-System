12fca624eab6e9d695fe317e4b799c0fffae1567
sahand <sahand.u99@gmail.com>
2023-10-30 11:54:18 +0330

    feat: admin in college app

--- college/admin.py
+++ college/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Faculty, FieldOfStudy, Term
 
-# Register your models here.
+
+@admin.register(Faculty)
+class FacultyAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(FieldOfStudy)
+class FacultyAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Term)
+class FacultyAdmin(admin.ModelAdmin):
+    pass
+
+# admin.site.register(Term)
