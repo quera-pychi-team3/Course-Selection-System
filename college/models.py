c42c8f79e0d164e329efbf52d646c56c7a005f18
sahand <sahand.u99@gmail.com>
2023-10-30 11:44:24 +0330

    refactor: change datetime fields to jalali date

--- college/models.py
+++ college/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django_jalali.db import models as jmodels
 
 # Create your models here.
 class Faculty(models.Model):
@@ -16,15 +16,15 @@ class FieldOfStudy(models.Model):
 
 class Term(models.Model):
     name = models.CharField(max_length=256)
-    students = models.ManyToManyField(to='accounts.models.Student', related_name='terms')
-    professors = models.ManyToManyField(to='accounts.models.Professor', related_name='terms')
-    TermCourses = models.ManyToManyField(to='courses.models.TermCourse', related_name='terms')
-    selection_start_time = models.DateTimeField()
-    selection_end_time = models.DateTimeField()
-    classes_start_time = models.DateTimeField()
-    classes_end_time = models.DateTimeField()
-    update_start_time = models.DateTimeField()
-    update_end_time = models.DateTimeField()
+    students = models.ManyToManyField(to='accounts.Student', related_name='terms')
+    professors = models.ManyToManyField(to='accounts.Professor', related_name='terms')
+    TermCourses = models.ManyToManyField(to='courses.TermCourse', related_name='terms')
+    selection_start_time = jmodels.jDateTimeField()
+    selection_end_time = jmodels.jDateTimeField()
+    classes_start_time = jmodels.jDateTimeField()
+    classes_end_time = jmodels.jDateTimeField()
+    update_start_time = jmodels.jDateTimeField()
+    update_end_time = jmodels.jDateTimeField()
     emergency_cancellation_end_time = models.DateTimeField()
-    exams_start_time = models.DateField()
-    term_end_time = models.DateField()
+    exams_start_time = jmodels.jDateField()
+    term_end_time = jmodels.jDateField()
