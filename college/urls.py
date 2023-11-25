15795984248c8564ea7745b299d2d4b41b9ad2a5
fatemeizadii <fatemeizadi26@ut.ac.ir>
2023-11-25 15:45:32 +0330

    termviewset

--- college/urls.py
+++ college/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views.faculty import FacultyViewSet
-from .views.term import TermViewSet, TermListAPIView, TermDetailAPIView, StudentCoursesAPIView
+from .views.term import TermViewSet, StudentCoursesAPIView
 
 app_name = 'college'
 
@@ -10,9 +10,8 @@ router.register(r'faculty', FacultyViewSet)
 router.register(r'terms', TermViewSet, basename='term')
 
 urlpatterns = [
-    path('terms/', TermListAPIView.as_view(), name='term-list'),
-    path('terms/<int:pk>/', TermDetailAPIView.as_view(), name='term-detail'),
+    path('terms/', TermViewSet.as_view(), name='term-list'),
+    path('terms/<int:pk>/', TermViewSet.as_view(), name='term-detail'),
     path('student/<str:pk>/my-courses/', StudentCoursesAPIView.as_view(), name='student-courses'),
     path('admin/', include(router.urls)),  
 ]
-
--- college/views/term.py
+++ college/views/term.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from drf_spectacular.utils import extend_schema
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework import viewsets
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
@@ -12,30 +14,37 @@ from accounts.models import Student, ITAdmin
 from accounts.permissions import IsStudent, IsProfessor, IsITAdmin
 
 
+
 @extend_schema(tags=["term"])
 class TermViewSet(viewsets.ModelViewSet):
-    permission_classes = [IsITAdmin, IsAuthenticated]
     queryset = Term.objects.all()
     serializer_class = TermSerializer
 
-    def perform_create(self, serializer):
-        serializer.save()
+    def get_permissions(self):
+        if self.action == 'list':
+            permission_classes = [IsStudent | IsProfessor]
+        elif self.action == 'create':
+            permission_classes = [IsITAdmin, IsAuthenticated]
+        else:
+            permission_classes = [IsStudent | IsProfessor]
+        return [permission() for permission in permission_classes]
 
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
 
-@extend_schema(tags=["term"])
-class TermListAPIView(generics.ListAPIView):
-    permission_classes = (IsStudent | IsProfessor,)
-    queryset = Term.objects.all()
-    serializer_class = TermSerializer
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
-
-@extend_schema(tags=["term"])
-class TermDetailAPIView(generics.RetrieveAPIView):
-    permission_classes = (IsStudent | IsProfessor,)
-    queryset = Term.objects.all()
-    serializer_class = TermSerializer
-    lookup_field = 'pk'
-
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
 
 @extend_schema(tags=["term"])
 class StudentCoursesAPIView(generics.ListAPIView):
